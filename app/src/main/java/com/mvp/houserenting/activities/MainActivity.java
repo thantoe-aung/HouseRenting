17512a449a4cf5e238cb78536b5dc8f251634b54
black-spirit <blackspirit781863@gmail.com>
2019-09-06 22:30:16 +0630

    Base URl Change

--- app/src/main/java/com/mvp/houserenting/activities/MainActivity.java
+++ app/src/main/java/com/mvp/houserenting/activities/MainActivity.java
@@ -1,25 +1,27 @@
 package com.mvp.houserenting.activities;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.mvp.houserenting.R;
 import com.mvp.houserenting.adapters.RecyclerItemAdapter;
 import com.mvp.houserenting.data.VOS.HouseVO;
 import com.mvp.houserenting.data.models.HouseModel;
-import com.mvp.houserenting.data.models.HouseModelImpl;
 import com.mvp.houserenting.delegates.HouseItemDelegate;
 
 import java.sql.Array;
@@ -45,66 +47,74 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @BindView(R.id.searchView)
     AppCompatAutoCompleteTextView searchView;
 
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout = findViewById(R.id.tabLayout);
         ButterKnife.bind(this);
+
+
+        progressBar.setVisibility(View.VISIBLE);
 
         imgGridView.setOnClickListener(this);
         imgListView.setOnClickListener(this);
 
-        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)),0,true);
-        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_near_me)),1,false);
-        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_price)),2,false);
-        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)),3,false);
-        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)),4,false);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)), 0, true);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_near_me)), 1, false);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_price)), 2, false);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)), 3, false);
+        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.tab_collection)), 4, false);
 
-        recyclerView=findViewById(R.id.recyclerView);
-        recyclerItemAdapter=new RecyclerItemAdapter(new HouseItemDelegate() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerItemAdapter = new RecyclerItemAdapter(new HouseItemDelegate() {
             @Override
             public void onTapHouseItem(int houseId) {
-                startActivity(DetailActivity.newIntent(getApplicationContext(),houseId));
+                startActivity(DetailActivity.newIntent(getApplicationContext(), houseId));
             }
 
             @Override
             public void onTapFabIcon(double latitude, double longitude) {
-                showMap(latitude,longitude);
+                showMap(latitude, longitude);
             }
         });
 
-        gridLayoutManager=new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        gridLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
 
 //        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(recyclerItemAdapter);
 
-       HouseModel.getHouses(new HouseModel.GetHousesFromDataLayerDelegate() {
-           @Override
-           public void onSuccess(List<HouseVO> houseList) {
-               recyclerItemAdapter.setNewData(houseList);
-               recyclerItemAdapter.notifyDataSetChanged();
-               nameArray=new String[houseList.size()];
-               for(int i=0;i<houseList.size();i++){
-                  nameArray[i]=houseList.get(i).getName();
-               }
+        HouseModel.getHouses(new HouseModel.GetHousesFromDataLayerDelegate() {
+            @Override
+            public void onSuccess(List<HouseVO> houseList) {
+                recyclerItemAdapter.setNewData(houseList);
+                recyclerItemAdapter.notifyDataSetChanged();
 
-               setUpAutoComplete();
-           }
+                progressBar.setVisibility(View.GONE);
+                nameArray = new String[houseList.size()];
+                for (int i = 0; i < houseList.size(); i++) {
+                    nameArray[i] = houseList.get(i).getName();
+                }
 
-           @Override
-           public void onFailure(String errorMessage) {
+                setUpAutoComplete();
+            }
 
-           }
-       });
+            @Override
+            public void onFailure(String errorMessage) {
+                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.actionGridView:
                 gridLayoutManager.setSpanCount(2);
                 break;
@@ -114,20 +124,21 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
-    private void setUpAutoComplete(){
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.select_dialog_item,nameArray);
+    private void setUpAutoComplete() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, nameArray);
         searchView.setThreshold(1);
         searchView.setAdapter(adapter);
     }
 
-    private void showMap(double latitude,double longitude){
-        String geoString="geo:"+latitude+","+longitude;
-        Uri uri=Uri.parse("google.navigation:q=16.779292,96.154465");
+    private void showMap(double latitude, double longitude) {
+//        String geoString = "geo:" + latitude + "," + longitude;
+        Uri uri = Uri.parse("google.navigation:q=16.779292,96.154465");
 //        "geo:37.7749,-122.4194"
-        Intent mapIntent=new Intent(Intent.ACTION_VIEW,uri);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
     }
+
 }
--- app/src/main/java/com/mvp/houserenting/utils/HouseConstants.java
+++ app/src/main/java/com/mvp/houserenting/utils/HouseConstants.java
@@ -1,7 +1,7 @@
 package com.mvp.houserenting.utils;
 
 public class HouseConstants {
-    public static final String BASE_URL="https://7f244e02-f9d4-460d-b6f2-5462053e21ff.mock.pstmn.io/";
+    public static final String BASE_URL="https://2d14f6f1-dfb9-49a0-b4ed-288cb1b0d585.mock.pstmn.io/";
     public static final String GET_HOUSES="/getAllHouses";
 
     public static final String EM_NULL_HOUSE_RESPONSE="Something Wrong";
